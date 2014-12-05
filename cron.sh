640dfff7fe5c07ffcec1db522cc199b66b4f8038
dequis <dx@dxzone.com.ar>
2014-12-05 06:34:04 -0300

    needlessly detailed server status

--- cron.sh
+++ cron.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-nmap -p 443,1863 bn1_a bn1_b bay_a bay_b db3_a db3_b -oX scan.xml> /dev/null
+
+regions=(ips_bay ips_bn1 ips_db3)
+
+for i in ${regions[@]}
+do
+    nmap -n -p 80,443,1863 -iL $i -oX $i.xml > /dev/null &
+done
+
+(
+    for i in $(cat ${regions[@]})
+    do
+        sh ping-msn.sh $i &
+    done
+) > ver_results
+
+wait
+
 python2 renderpage.py
--- renderpage.py
+++ renderpage.py
@@ -3,34 +3,43 @@ import os
 import xml.etree.ElementTree as ET
 from jinja2 import Template
 
-SCAN_XML = 'scan.xml'
+ver_results = [x.strip() for x in open("ver_results").readlines()]
 
-def parse(filename):
-    tree = ET.parse(filename)
-    hostnames = {}
+def parse(region):
+    tree = ET.parse("ips_%s.xml" % region)
     host_ports = {}
 
     for host in tree.getroot().findall("host"):
-        hostname = host.find("hostnames")[0].attrib['name']
-        address = host.find("address").attrib['addr']
-        hostnames[hostname] = address
-        for port in host.find("ports"):
-            portid = int(port.attrib['portid'])
-            state = port.find("state").attrib['state'] == 'open'
-            host_ports.setdefault(hostname, {})[portid] = state
+        hostname = host.find("address").attrib['addr']
+        ports = host.find("ports")
+        if ports is None:
+            host_ports[hostname] = {80: False, 443: False, 1863: False}
+        else:
+            for port in ports:
+                portid = int(port.attrib['portid'])
+                state = port.find("state").attrib['state'] == 'open'
+                host_ports.setdefault(hostname, {})[portid] = state
 
     output = []
-    for hostname, ports in sorted(host_ports.items()):
-        output.append((hostname, hostnames[hostname], ports.values()))
-    return output
+    for line in open("ips_%s" % region):
+        ip = line.strip()
+        ver_result = ip not in ver_results
+        if ip in host_ports:
+            output.append((ip, host_ports[ip].values(), ver_result))
 
-def render(servers):
+    return (region, output)
+
+def render(regions):
     template = Template(open("template.html").read().decode("utf-8"))
-    output = template.render(servers=servers)
+    output = template.render(regions=regions)
     open("index.html", "w").write(output.encode("utf-8"))
 
 def main():
-    render(parse(SCAN_XML))
+    render([
+        parse('bay'),
+        parse('bn1'),
+        parse('db3'),
+    ])
 
 if __name__ == '__main__':
     main()
