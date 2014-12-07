4ddf09a865124406772f7cb2234610fa30c6780e
dequis <dx@dxzone.com.ar>
2014-12-07 01:50:12 -0300

    patience is a virtue

--- cron.sh
+++ cron.sh
@@ -5,13 +5,14 @@ regions=(ips_bay ips_bn1 ips_db3)
 
 for i in ${regions[@]}
 do
-    nmap -n -p 80,443,1863 -iL $i -oX $i.xml > /dev/null &
+    nmap -n -p 80,443,1863 --min-rtt-timeout 3000ms -iL $i -oX $i.xml > /dev/null
 done
 
 (
     for i in $(cat ${regions[@]})
     do
         sh ping-msn.sh $i &
+        sleep 0.01
     done
 ) > ver_results
 
--- ping-msn.sh
+++ ping-msn.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # prints the IP if the server is down
-if [ -z "$(echo VER 1 MSNP18 | nc -w 5 $1 1863)" ]; then
+if [ -z "$(echo VER 1 MSNP18 | nc -w 10 $1 1863)" ]; then
     echo $1
 fi
