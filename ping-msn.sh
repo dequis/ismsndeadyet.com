ca1f4bc9c35af1d9bfb0980bf3fd024b3f36b7aa
dequis <dx@dxzone.com.ar>
2015-03-06 02:03:50 -0300

    https based protocol version testing script

--- ping-msn.sh
+++ ping-msn.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # usage: ping-msn.sh <version> <ip>
 # prints the IP if the server doesn't support the protocol
-if [ -z "$(echo VER 1 $1 | nc -w 10 $2 1863 | grep $1)" ]; then
-    echo $2
-fi
+# otherwise, quits silently
+curl -sk -m10 "https://$2/gateway/gateway.dll?Action=open" -d "VER 1 $1"$'\r\n' | grep -q $1 || echo $2
